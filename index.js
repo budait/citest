f3dcbb52cc55338847134180fd08dffdd34f40bb
budait <budait@maxwell.sze.hu>
2018-07-17 08:45:01 +0200

    bt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello ci World!'))
+app.get('/', (req, res) => res.send('Hello cib World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
